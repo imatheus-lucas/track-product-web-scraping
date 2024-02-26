86888293c39f1ec76ed409254677e3fc59fea01a
imatheus-lucas <matheeuslucas13@gmail.com>
2024-02-26 15:50:02 -0300

    remove undesnecessary fields

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -5,8 +5,6 @@ import prisma from '@/lib/prisma';
 import { scrape } from '@/lib/scrape';
 import { getAveragePrice, getHighestPrice, getLowestPrice } from '@/lib/utils';
 import { NextResponse } from 'next/server';
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
 
 export async function GET() {
     const products = await prisma.products.findMany({
