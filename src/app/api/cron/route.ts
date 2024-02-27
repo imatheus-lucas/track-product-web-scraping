85ca84a1c17b5de8ce59fc8aec7e57900b508e0d
imatheus-lucas <matheeuslucas13@gmail.com>
2024-02-27 10:35:34 -0300

    Force dynamic and revalidate has been added

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -5,7 +5,8 @@ import prisma from '@/lib/prisma';
 import { scrape } from '@/lib/scrape';
 import { getAveragePrice, getHighestPrice, getLowestPrice } from '@/lib/utils';
 import { NextResponse } from 'next/server';
-
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 export async function GET() {
     const products = await prisma.products.findMany({
         where: {
